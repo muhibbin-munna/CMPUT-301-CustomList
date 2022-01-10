02456dfb1b9187a49dabd9253aeb3fd5e95bad33
Md. Muhibbin Hossin Munna <muhibbin.munna2017@gmail.com>
2022-01-11 00:29:07 +0600

    commit 1707081

---
+++ app/src/main/java/com/example/simpleparadox/listycity/CityList.java
@@ -0,0 +1,51 @@
+package com.example.simpleparadox.listycity;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * This is a class that keeps track of a list of city objects
+ */
+public class CityList {
+    private List<City> cities = new ArrayList<>();
+
+    /**
+     * This adds a city to the list if the city does not exist
+     * @param city
+     *      This is a candidate city to add
+     */
+    public void add(City city) {
+        if (cities.contains(city)) {
+            throw new IllegalArgumentException();
+        }
+        cities.add(city);
+    }
+
+    /**
+     * This returns a sorted list of cities
+     * @return
+     *      Return the sorted list
+     */
+    public List<City> getCities() {
+        List<City> list = cities;
+        Collections.sort(list);
+        return list;
+    }
+
+    /**
+     * This delete a city to the list if the city exists
+     * @param city
+     *      This is a candidate city to delete
+     */
+    public void deleteCity(City city) {
+        if (cities.contains(city)) {
+            cities.remove(city);
+
+        }
+        else{
+            throw new IllegalArgumentException();
+        }
+
+    }
+}
